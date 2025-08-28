0a506a54f3e9d39d472811b38cd6d3a5e8204636
David Carroll <david@hazeltine.co>
2025-08-28 10:21:19 -0700

    98dvw

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -882,7 +882,7 @@ function WeeklyMatchesPage() {
         </div>
       </div>
 
-      <div className="w-[100dvw] mt-8 2xl:text-8xl xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-3xl leading-none text-center font-bold text-black uppercase mix-blend-soft-light">
+      <div className="w-[98dvw] mt-8 2xl:text-8xl xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-3xl leading-none text-center font-bold text-black uppercase mix-blend-soft-light">
         Long Live The Clipboard
       </div>
 
